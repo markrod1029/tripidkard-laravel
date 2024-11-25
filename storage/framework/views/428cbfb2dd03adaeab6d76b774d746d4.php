232d5452b78c50a27d7979844b3a67d222eb2655
markrod1029 <markrodcadayong17@gmail.com>
2024-11-25 16:44:37 +0800

    title of tripidkard

--- storage/framework/views/428cbfb2dd03adaeab6d76b774d746d4.php
+++ storage/framework/views/428cbfb2dd03adaeab6d76b774d746d4.php
@@ -1,26 +1,22 @@
-
 <!DOCTYPE html>
-
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Aeonsprint Solution</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TripidKard</title>
 
-<?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+    <link rel="icon" href="<?php echo e(asset('/storage/img/logo.jpg')); ?>" type="image/x-icon">
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+</head>
 
+<body class="hold-transition sidebar-mini">
 
-<body class="sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed menu-open  sidebar-mini ">
-    
     <div class="wrapper" id="app">
-    
-        <router-view>
-            
-        </router-view>
-   
+        <router-view></router-view>
     </div>
 
-
 </body>
+
 </html>
 <?php /**PATH C:\xampp\htdocs\laravel-project\tripidkard-laravel\resources\views/admin/app.blade.php ENDPATH**/ ?>
